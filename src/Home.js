b8fbca67a45a26e5cead974369b4ab17e36c9b33
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2024-02-26 12:45:48 +0700

    responsive text

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import './Style/StyleHome.css';
 
 import Ilustrasi from './Assets/ilustrasi2.png'
 
@@ -9,11 +10,11 @@ const Home = () => {
     return(
         <div>
             <div className = "mt-[85px] text-center" >
-                <h1 className="text-[30px] font-bold"
+                <h1 className="title text-[30px] font-bold"
                 data-aos="fade-down"
                 data-aos-duration="1500"><span className="text-[#3C66D4] text-[50px]">GC</span> Tools</h1>
-                
-                <p className="text-[18px] px-[30px] mt-[10px]" 
+
+                <p className="description text-[18px] px-[30px] mt-[10px]" 
                 data-aos="fade-down"
                 data-aos-duration="1000">Make your website creation experience enjoyable with free tools.</p>
             </div>
