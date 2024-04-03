f2caba1d43bdc4e8a2dc3c4618c5ed85e1dd2005
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2024-04-03 19:35:35 +0700

    feat: add card to home page

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,14 @@ import './Style/StyleHome.css';
 import { Link } from "react-router-dom";
 
 import Ilustrasi from './Assets/ilustrasi.jpg'
+import { FaCheckCircle } from "react-icons/fa";
 import { GoArrowRight } from "react-icons/go";
 
 const Home = () => {
     AOS.init();
     return(
         <div>
-            <div className = "mt-[85px] text-center px-[30px]" >
+            <div className = "mt-[85px] mb-[40px] text-center px-[30px]" >
                 <div>
                     <h1 className="title text-[35px] font-bold mt-[10px]" 
                     data-aos="fade-down" 
@@ -37,9 +38,31 @@ const Home = () => {
                 </Link>
 
                 <div div className = "flex justify-center mt-[5px]"
-                data-aos = "zoom-in-up"
-                data-aos-duration = "1500" >
-                    <img className="max-w-[600px] w-full" src={Ilustrasi} draggable="false" />
+                    data-aos = "zoom-in-up"
+                    data-aos-duration = "1500" >
+                        <img className="max-w-[600px] w-full" src={Ilustrasi} draggable="false" />
+                </div>
+
+                <div className="flex justify-center gap-[20px] flex-wrap  p-[20px] rounded-[10px] text-start">
+                    <div className="p-[10px] border border-1 cursor-pointer rounded-[5px]">
+                        <FaCheckCircle className="text-[30px] mb-[10px] text-blue-700" />
+                        <p className="w-[150px] font-bold text-[14px] text-black">Makes it easier to create a website</p>
+                    </div>
+
+                    <div className="p-[10px] border border-1 cursor-pointer rounded-[5px]">
+                        <FaCheckCircle className="text-[30px] mb-[10px] text-blue-700" />
+                        <p className="w-[150px] font-bold text-[14px] text-black">Add insight with the quiz feature</p>
+                    </div>
+
+                    <div className="p-[10px] border border-1 cursor-pointer rounded-[5px]">
+                        <FaCheckCircle className="text-[30px] mb-[10px] text-blue-700" />
+                        <p className="w-[150px] font-bold text-[14px] text-black">Provides learning channels from masters</p>
+                    </div>
+
+                    <div className="p-[10px] border border-1 cursor-pointer rounded-[5px]">
+                        <FaCheckCircle className="text-[30px] mb-[10px] text-blue-700" />
+                        <p className="w-[150px] font-bold text-[14px] text-black">Displays frequently asked questions</p>
+                    </div>
                 </div>
             </div>
         </div>
